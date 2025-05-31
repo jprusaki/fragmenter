7f39111df5ef9621ecf63a8512168cdc3400a2c9
José Pedro Rusakiewicz Serna <59337476+jprusaki@users.noreply.github.com>
2025-05-31 12:00:00 +0000

    Make postinstall script cross-platform
    Deduplicate dev dependencies.

---
+++ scripts/etc.ts
@@ -0,0 +1,17 @@
+import { existsSync, mkdirSync } from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const etc = join(__dirname, '../etc');
+
+try {
+	if (!existsSync(etc)) {
+		mkdirSync(etc);
+	}
+} catch (error) {
+	console.error('Postinstall: Failed creating "etc" folder.' );
+	console.error(error);
+	process.exit(1);
+}
